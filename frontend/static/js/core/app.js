5d2bf9a71d9ac374846ad715e6987ef73a50c3a6
The Ghost <jeandre.greyling@gmail.com>
2026-04-16 08:53:40 +1000

    [2026-04-15 22:53:40] Closed TEST-DRYRUN-1776293620

--- frontend/static/js/core/app.js
+++ frontend/static/js/core/app.js
@@ -115,6 +115,7 @@ function openWindow(id, title, templateId) {
             else if (id === 'agents-config') loadAgentsConfigData && loadAgentsConfigData(win);
             else if (id === 'localai') initializeLocalAiPanel && initializeLocalAiPanel();
             else if (id === 'trace') initTraceView && initTraceView(win);
+            else if (id === 'onboarding') _initOnboarding && _initOnboarding();
             
             _troubleshootLog && _troubleshootLog('info', 'Window opened', `id=${windowKey} base=${id}`);
         } catch (err) {
